ecbb193533ffa85cf97dbe028346059858f787b1
Projjol Banerji <projjol@socialcops.org>
2015-10-03 23:56:21 +0530

    login added

--- app/views.py
+++ app/views.py
@@ -1,21 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'proj'}  # fake user
-    posts = [  # fake array of posts
-        { 
-            'author': {'nickname': 'Prof1'}, 
-            'body': 'Call for research assistance' 
-        },
-        { 
-            'author': {'nickname': 'Stud1'}, 
-            'body': 'Interested in doing research for X' 
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
